a621dd9fc4fc92b25871893a11fc44ec61f196ff
Steven Roland <steven@mojave.io>
2016-04-22 21:12:56 -0400

    Routes
    - Created routes.
    - Renamed the routes file for clarity.

---
+++ resource/UIroutes.js
@@ -0,0 +1,41 @@
+'use strict';
+var express = require('express');
+
+var ui      = express.Router();
+
+ui.route('/')
+  .get(function(req, res) {
+    res.render('index.ejs');
+  });
+
+ui.route('/login')
+  .get(function(req, res) {
+    res.render('account/login.ejs', { message: req.flash('loginMessage') });
+  });
+
+ui.route('/register')
+  .get(function(req, res) {
+    res.render('account/register.ejs', { message: req.flash('registerMessage') });
+  });
+
+ui.route('/profile')
+  .get(isLoggedIn, function(req, res) {
+    res.render('account/profile.ejs', {
+      user : req.user
+    });
+  });
+
+ui.route('/logout')
+  .get(function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
+
+module.exports = ui;
--- server.js
+++ server.js
@@ -14,23 +14,24 @@ var morgan       = require('morgan');
 var app          = express();
 var port         = process.env.PORT || 3000;
 var config       = require('./resource/config');
+var ui           = require('./resource/UIroutes.js');
 //                   require('./resource/config')(passport);
 
-// mongoose.connect(config.dbURL);
+mongoose.connect(config.dbURL);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser());
 
 app.set('view engine', 'ejs');
+app.set('views', './base/templates')
 
-app.use(session({ secret: config.secret }));
+app.use(session({'secret': config.secret}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-// ------ Build the routes
-// require('./resource/routes.js')(app, passport);
+app.use('/', ui);
 
 // ------ Serve
 app.listen(port, function() {
